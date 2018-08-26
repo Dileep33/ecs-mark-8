fd28a4bfa4e8d99e67268381d7fa767c1de8a68c
Libin V Babu <libinvbabu@gmail.com>
2018-08-26 14:44:30 +0530

    Updating todays chat slug

--- src/components/ChatStrip.vue
+++ src/components/ChatStrip.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="chat-stories">
-        <router-link :to="'/chat-story/' + firstChatStorySlug" @click.native="triggerChatStripAnalytics()">
+        <router-link :to="'/chat-story/' + ChatStorySlugToday" @click.native="triggerChatStripAnalytics()">
             <span>__("chatstory_strip")</span>
             <i class="material-icons">keyboard_arrow_right</i>
         </router-link>
@@ -52,8 +52,9 @@ export default {
         }
     },
     created() {
-        const firstChatStory = chatStories[Object.keys(chatStories)[0]];
-        this.firstChatStorySlug = firstChatStory['url-slug'];
+        // change the story key everyday
+        const ChatStoryToday = chatStories[Object.keys(chatStories)[11]];
+        this.ChatStorySlugToday = ChatStoryToday['url-slug'];
     },
     watch: {
         'inViewport.now'(visible) {
