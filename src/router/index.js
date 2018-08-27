e42ba00ec5a0a93d258b2eec2dbeeab999103b9d
Libin V Babu <libinvbabu@gmail.com>
2018-08-27 18:27:37 +0530

    CHanging buckets for reader

--- src/router/index.js
+++ src/router/index.js
@@ -365,7 +365,7 @@ var router = new Router({
                 let bucketId = getCookie('bucket_id') ? getCookie('bucket_id') : 42;
                 console.log("bucket id ", bucketId);
 
-                if (bucketId > 40 && bucketId <= 70 && process.env.LANGUAGE === 'hi') {
+                if (bucketId > 10 && bucketId <= 70 && process.env.LANGUAGE === 'hi') {
                     return import ('@/pages/experiments/rating_stickers_v1/Reader.vue');
                 } else {
                     return new Promise((resolve, reject) => resolve(ReaderPageComponent));
@@ -382,7 +382,7 @@ var router = new Router({
                 let bucketId = getCookie('bucket_id') ? getCookie('bucket_id') : 42;
                 console.log("bucket id ", bucketId);
 
-                if (bucketId > 40 && bucketId <= 70 && process.env.LANGUAGE === 'hi') {
+                if (bucketId > 10 && bucketId <= 70 && process.env.LANGUAGE === 'hi') {
                     return import ('@/pages/experiments/rating_stickers_v1/ReaderV2.vue');
                 } else {
                     return new Promise((resolve, reject) => resolve(ReaderPageV2Component));
