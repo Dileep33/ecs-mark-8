4f14b4e332a6686be7bc97fcdb76842dda6d23b7
Libin V Babu <libinvbabu@gmail.com>
2018-08-27 14:31:25 +0530

    Updating chatstrip start url

--- src/components/ChatStrip.vue
+++ src/components/ChatStrip.vue
@@ -53,7 +53,7 @@ export default {
     },
     created() {
         // change the story key everyday
-        const ChatStoryToday = chatStories[Object.keys(chatStories)[11]];
+        const ChatStoryToday = chatStories[Object.keys(chatStories)[15]];
         this.ChatStorySlugToday = ChatStoryToday['url-slug'];
     },
     watch: {
