a50f9f8f942d0c25089d5520494c7b52e99207e2
Roshan Alexander <roshanalexander88@gmail.com>
2018-08-29 17:09:29 +0530

    Added changes to place holders

--- src/pages/EventParticipate.vue
+++ src/pages/EventParticipate.vue
@@ -53,7 +53,7 @@
                                     </div>
                                     <div class="form-group">
                                         <label for="pratilipi_write_title_en_input">__("writer_input_title_en")</label>
-                                        <input type="text" :value="titleEn" @input="($event) => { titleEn = $event.target.value}" class="form-control" id="pratilipi_write_title_en_input" :placeholder="'__("writer_input_title_en")'">
+                                        <input type="text" :value="titleEn" @input="($event) => { titleEn = $event.target.value}" class="form-control" id="pratilipi_write_title_en_input" placeholder="__('writer_input_title_en')">
                                     </div>
                                     <div class="form-group">
                                         <label for="pratilipi_write_type">__("write_type")</label>
