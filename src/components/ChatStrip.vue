2aabc09b83cca7f6ebeb0c333582d91418edecfb
Libin V Babu <libinvbabu@gmail.com>
2018-08-31 11:52:47 +0530

    Updating chat stories for day 8

--- src/components/ChatStrip.vue
+++ src/components/ChatStrip.vue
@@ -53,7 +53,7 @@ export default {
     },
     created() {
         // change the story key everyday
-        const ChatStoryToday = chatStories[Object.keys(chatStories)[27]];
+        const ChatStoryToday = chatStories[Object.keys(chatStories)[30]];
         this.ChatStorySlugToday = ChatStoryToday['url-slug'];
     },
     watch: {
