bd8ac588eabb93362ab8dd3e1e8d4d16cce9d411
Libin V Babu <libinvbabu@gmail.com>
2018-09-25 15:42:44 +0530

    PP-182 Upadting Share Modal title text

--- src/layout/Reader-layout.vue
+++ src/layout/Reader-layout.vue
@@ -2,13 +2,17 @@
     <div>
         <slot></slot>
         <LoginModal></LoginModal>
-        <ShareModal></ShareModal>
+        <ShareModalOne v-if="getCookie('bucket_id') > 70 && getCookie('bucket_id') <= 85"></ShareModalOne>
+        <ShareModalTwo v-else-if="getCookie('bucket_id') > 85 && getCookie('bucket_id') <= 100"></ShareModalTwo>
+        <ShareModal v-else></ShareModal>
         <Alert :isReader="true"></Alert>
     </div>
 </template>
 
 <script>
 import LoginModal from '@/components/LoginModal.vue';
+import ShareModalOne from '@/components/experiments/share_ui/Share_v1.vue';
+import ShareModalTwo from '@/components/experiments/share_ui/Share_v2.vue';
 import ShareModal from '@/components/Share.vue';
 import Alert from '@/components/Alert.vue';
 import constants from '@/constants';
@@ -19,6 +23,8 @@ import { mapGetters, mapActions } from 'vuex'
 export default {
     components: {
         LoginModal,
+        ShareModalOne,
+        ShareModalTwo,
         ShareModal,
         Alert
     },
--- src/layout/main-layout.vue
+++ src/layout/main-layout.vue
@@ -8,7 +8,9 @@
         <slot></slot>
         <PratilipiModal></PratilipiModal>
         <LoginModal></LoginModal>
-        <ShareModal></ShareModal>
+        <ShareModalOne v-if="getCookie('bucket_id') > 70 && getCookie('bucket_id') <= 85"></ShareModalOne>
+        <ShareModalTwo v-else-if="getCookie('bucket_id') > 85 && getCookie('bucket_id') <= 100"></ShareModalTwo>
+        <ShareModal v-else></ShareModal>
         <InputModal></InputModal>
         <MultiInputModal></MultiInputModal>
         <ConfirmationModal></ConfirmationModal>
@@ -25,6 +27,8 @@ import AppBannerTwo from '@/components/experiments/appbanner_v2/AppBanner.vue';
 import AppBannerThree from '@/components/experiments/appbanner_v3/AppBanner.vue';
 import PratilipiModal from '@/components/PratilipiModal.vue';
 import LoginModal from '@/components/LoginModal.vue';
+import ShareModalOne from '@/components/experiments/share_ui/Share_v1.vue';
+import ShareModalTwo from '@/components/experiments/share_ui/Share_v2.vue';
 import ShareModal from '@/components/Share.vue';
 import InputModal from '@/components/InputModal.vue';
 import MultiInputModal from '@/components/MultiInputModal.vue';
@@ -69,6 +73,8 @@ export default {
         AppBannerThree,
         PratilipiModal,
         LoginModal,
+        ShareModalOne,
+        ShareModalTwo,
         ShareModal,
         InputModal,
         MultiInputModal,
