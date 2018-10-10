f273b81bbf85e325c8356e2c62324fa481a7bd0a
Libin V Babu <libinvbabu@gmail.com>
2018-10-10 17:14:36 +0530

    Adding custom text for rate and review

--- src/components/experiments/applogin_v1/AppLogin.vue
+++ src/components/experiments/applogin_v1/AppLogin.vue
@@ -6,6 +6,7 @@
                 <br>
                 <span v-if="getPostLoginAction.action && getPostLoginAction.action.indexOf('addToLibrary') > -1">__('login_popup_story_added_to_library')</span>
                 <span v-else-if="getPostLoginAction.action && getPostLoginAction.action.indexOf('follow') > -1">__("login_popup_follow_unfollow")</span>
+                <span v-else-if="getPostLoginAction.action && getPostLoginAction.action.indexOf('saveOrUpdateReview') > -1">__("login_popup_read_and_rate_stories")</span>
                 <span v-else>__("login_popup_default")</span>
             </div>
             <div class="social-login">
