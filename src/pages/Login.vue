2bd76c51aefd581263f87bd34c6b867c35d37d54
Libin V Babu <libinvbabu@gmail.com>
2018-10-12 15:19:50 +0530

    Updating the custom texts on login page

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -17,9 +17,11 @@
                             <RegisterWithCustomMessage
                                 :currentStep="currentStep"
                                 :changeCurrentStep="changeCurrentStep"
+                                v-if="getCookie('bucket_id') > 70 && getCookie('bucket_id') < 100"
+                                
                             ></RegisterWithCustomMessage>
                             
-                            <!-- <Register :currentStep="currentStep" :changeCurrentStep="changeCurrentStep" v-else></Register> -->
+                            <Register :currentStep="currentStep" :changeCurrentStep="changeCurrentStep" v-else></Register>
 
                             <!-- Modal -->
                             <!--<ForgetPasswordModal></ForgetPasswordModal>-->
@@ -45,7 +47,7 @@
 import MainLayout from '@/layout/main-layout.vue';
 import Spinner from '@/components/Spinner.vue';
 import Login from '@/components/Login.vue';
-// import Register from '@/components/AppLogin.vue';
+import Register from '@/components/AppLogin.vue';
 import RegisterWithCustomMessage from '@/components/experiments/applogin_v1/AppLogin';
 // import ForgetPasswordModal from '@/components/ForgetPasswordModal.vue';
 import mixins from '@/mixins';
@@ -59,7 +61,7 @@ export default {
         MainLayout,
         Spinner,
         Login,
-        // Register,
+        Register,
         RegisterWithCustomMessage
         // ForgetPasswordModal
     },
