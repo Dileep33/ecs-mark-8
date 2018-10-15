189e2a420054a3f956d527d3d9a31fe3c027db42
Libin V Babu <libinvbabu@gmail.com>
2018-10-15 17:49:07 +0530

    Adding fb signup below read button

--- src/pages/Pratilipi.vue
+++ src/pages/Pratilipi.vue
@@ -116,6 +116,11 @@
                                   <span>__("read")</span>
                                 </router-link>
                             </div>
+                            
+                            <!-- Facebook Individual SignUp -->
+                            <FacebookLogin
+                            :directBtn="true"
+                            v-if="this.getUserDetails.isGuest && (getCookie('bucket_id') > 70 && getCookie('bucket_id') <=99)"></FacebookLogin>
 
                             <BookShareStripV1
                             v-if="getCookie('bucket_id') > 70 && getCookie('bucket_id') <= 85"
@@ -304,6 +309,7 @@ import VapasiQuote from '@/components/VapasiQuote.vue';
 import VapasiHoroscope from '@/components/VapasiHoroscope.vue';
 import VapasiJoke from '@/components/VapasiJoke.vue';
 import PratilipiPublishShareModal from '@/components/PratilipiPublishShareModal.vue';
+import FacebookLogin from '@/components/FacebookLogin';
 
 export default {
     name: 'Pratilipi',
@@ -711,6 +717,7 @@ export default {
         VapasiJoke,
         NextPratilipiStrip,
         PratilipiPublishShareModal,
+        FacebookLogin
     },
     mounted() {
         window.addEventListener('scroll', this.updateScroll);
