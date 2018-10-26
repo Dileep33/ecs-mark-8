00e28aeebf0ec057a70be96bfae8cfbf19440448
Libin V Babu <libinvbabu@gmail.com>
2018-10-26 13:07:00 +0530

    Adding Sign up strip to the readers

--- src/pages/Reader.vue
+++ src/pages/Reader.vue
@@ -294,6 +294,7 @@
                 </div>
             </div>
             <OpenInApp v-if="isAndroid() && percentScrolled < 102 && getPratilipiLoadingState === 'LOADING_SUCCESS' && (getCookie('bucket_id') > 40 && getCookie('bucket_id') <= 99)" :isVisible="shouldShowOpenInAppStrip" :pratilipiData="getPratilipiData"></OpenInApp>
+            <SignUpStrip v-if="getPratilipiLoadingState === 'LOADING_SUCCESS' && (getCookie('bucket_id') > 10 && getCookie('bucket_id') <= 40)" :pratilipiData="getPratilipiData"></SignUpStrip>
             <div class="overlay" @click="closeSidebar"></div>
             <div class="overlay-1" @click="closeReviewModal"></div>
             <div class="overlay-2" @click="closeRatingModal"></div>
@@ -318,6 +319,7 @@ import WebPushStrip from '@/components/WebPushStrip.vue';
 import WebPushModal from '@/components/WebPushModal.vue';
 import Recommendation from '@/components/Recommendation.vue';
 import OpenInApp from '@/components/OpenInApp.vue';
+import SignUpStrip from '@/components/SignUpStrip.vue';
 import ShareStrip from '@/components/ShareStrip.vue';
 import NextPratilipiStrip from '@/components/NextPratilipiStrip.vue'
 import WebPushUtil from '@/utils/WebPushUtil';
@@ -335,6 +337,7 @@ export default {
         Recommendation,
         ShareStrip,
         OpenInApp,
+        SignUpStrip,
         NextPratilipiStrip
     },
     mixins: [
--- src/pages/experiments/reader_redesign/ReaderV2WithSignIn1.vue
+++ src/pages/experiments/reader_redesign/ReaderV2WithSignIn1.vue
@@ -299,6 +299,7 @@
             </div>
 
             <OpenInApp v-if="isAndroid() && readerPercentScrolled < 102 && getPratilipiLoadingState === 'LOADING_SUCCESS' && (getCookie('bucket_id') > 40 && getCookie('bucket_id') <= 99)" :isVisible="shouldShowOpenInAppStrip" :pratilipiData="getPratilipiData"></OpenInApp>
+            <SignUpStrip v-if="getPratilipiLoadingState === 'LOADING_SUCCESS' && (getCookie('bucket_id') > 10 && getCookie('bucket_id') <= 40)" :pratilipiData="getPratilipiData"></SignUpStrip>
             <div class="overlay" @click="closeSidebar"></div>
             <div class="overlay-1" @click="closeReviewModal"></div>
             <div class="overlay-2" @click="closeRatingModal"></div>
@@ -323,6 +324,7 @@ import WebPushStrip from '@/components/WebPushStrip.vue';
 import WebPushModal from '@/components/WebPushModal.vue';
 import Recommendation from '@/components/Recommendation.vue';
 import OpenInApp from '@/components/OpenInApp.vue';
+import SignUpStrip from '@/components/SignUpStrip.vue';
 import ShareStrip from '@/components/ShareStrip.vue';
 import NextPratilipiStrip from '@/components/NextPratilipiStrip.vue'
 import ServerError from '@/components/ServerError.vue';
@@ -362,6 +364,7 @@ export default {
         Recommendation,
         ShareStrip,
         OpenInApp,
+        SignUpStrip,
         NextPratilipiStrip,
         ServerError,
         TranslatingInputTextArea,
