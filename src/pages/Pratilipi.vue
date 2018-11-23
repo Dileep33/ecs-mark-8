ecf779663bc5e8e20138613ebacd24dd036eb932
Suryadeep Pal <suryadeep10@gmail.com>
2018-11-23 13:00:01 +0530

    [ADD] Added recommendation experiment to story page

--- src/pages/Pratilipi.vue
+++ src/pages/Pratilipi.vue
@@ -152,7 +152,7 @@
 
 <script>
 import MainLayout from '@/layout/main-layout.vue';
-import Recommendation from '@/components/Recommendation.vue';
+import Recommendation from '@/components/experiments/recommendation_v2/Recommendation.vue';
 import AboutAuthor from '@/components/AboutAuthor.vue';
 import Spinner from '@/components/Spinner.vue';
 import Reviews from '@/components/Reviews.vue';
